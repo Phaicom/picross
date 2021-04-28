70aea7b586122c68dd1184df8164e92f31fc2ed8
Phaicom <reawpai.chun@yahoo.com>
2021-04-28 22:21:33 +0700

    chore: add pug and prettier support

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,19 @@
-<template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">orga-board</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+<template lang="pug">
+.container
+  div
+    Logo
+    h1.title orga-board
+    .links
+      a.button--green(
+        href='https://nuxtjs.org/',
+        target='_blank',
+        rel='noopener noreferrer'
+      ) Documentation
+      a.button--grey(
+        href='https://github.com/nuxt/nuxt.js',
+        target='_blank',
+        rel='noopener noreferrer'
+      ) GitHub
 </template>
 
 <script lang="ts">
@@ -31,7 +22,7 @@ import Vue from 'vue'
 export default Vue.extend({})
 </script>
 
-<style>
+<style lang="scss" scoped>
 .container {
   margin: 0 auto;
   min-height: 100vh;
@@ -39,27 +30,28 @@ export default Vue.extend({})
   justify-content: center;
   align-items: center;
   text-align: center;
-}
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+  .title {
+    font-family: 'Quicksand', 'Source Sans Pro', -apple-system,
+      BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial,
+      sans-serif;
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
+  }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  .subtitle {
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+  }
 
-.links {
-  padding-top: 15px;
+  .links {
+    padding-top: 15px;
+  }
 }
 </style>
