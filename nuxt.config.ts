e9871d10eaff2a6b84b3303b0a15d95f7dff8062
Phaicom <reawpai.chun@yahoo.com>
2024-02-20 13:07:27 +0700

    chore: update test libraries

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@unocss/nuxt', '@nuxtjs/color-mode', '@pinia/nuxt', 'nuxt-vitest'],
+  modules: [
+    '@unocss/nuxt',
+    '@nuxtjs/color-mode',
+    '@pinia/nuxt',
+    '@nuxt/test-utils/module',
+  ],
   colorMode: {
     classSuffix: '',
   },
