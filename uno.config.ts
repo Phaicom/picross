cf50bc3e4a9599510ab2f2b9b32b86e0ad8e9673
Phaicom <reawpai.chun@yahoo.com>
2024-02-28 07:42:22 +0700

    chore: remove transform from root uno config

--- uno.config.ts
+++ uno.config.ts
@@ -1,13 +1,9 @@
 import {
   defineConfig,
-  transformerDirectives,
 } from 'unocss'
 
 import preset from './apps/web/uno.preset'
 
 export default defineConfig({
   presets: [preset],
-  transformers: [
-    transformerDirectives(),
-  ],
 })
