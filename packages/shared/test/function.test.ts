82002f0d79b30bf578f737859558b7fa5b2e95f9
Phaicom <reawpai.chun@yahoo.com>
2024-03-01 07:00:32 +0700

    refactor: optimize shared function test

--- packages/shared/test/function.test.ts
+++ packages/shared/test/function.test.ts
@@ -3,20 +3,12 @@ import { combination, last, range } from '../src'
 
 describe('shared range function', () => {
   it('range [0, 5]', () => {
-    const resultItr = range(0, 5)
-    const result = []
-    for (const value of resultItr)
-      result.push(value)
-
+    const result = Array.from(range(0, 5))
     expect(result).toStrictEqual([0, 1, 2, 3, 4, 5])
   })
 
   it('range [3, 5]', () => {
-    const resultItr = range(3, 5)
-    const result = []
-    for (const value of resultItr)
-      result.push(value)
-
+    const result = Array.from(range(3, 5))
     expect(result).toStrictEqual([3, 4, 5])
   })
 })
@@ -25,25 +17,20 @@ describe('shared last function', () => {
   it('last number', () => {
     const input = [1, 5, 77]
     const result = last(input)
-
-    expect(result).toStrictEqual(77)
+    expect(result).toBe(77)
   })
 
   it('last string', () => {
     const input = ['lorem', 'ipsum', 'dolor', 'sit', 'amet']
     const result = last(input)
-
-    expect(result).toStrictEqual('amet')
+    expect(result).toBe('amet')
   })
 })
 
 describe('shared combination function', () => {
   it('combination A-Za-z', () => {
     const input = 'ABCDEFGHIJKLMNOPQRSTWXYZabcdefghijklmnopqrstwxyz'.split('')
-    let result = 0
-    for (const _ of combination(input, 2))
-      result += 1
-
-    expect(result).toStrictEqual(1128)
+    const result = Array.from(combination(input, 2)).length
+    expect(result).toBe(1128)
   })
 })
