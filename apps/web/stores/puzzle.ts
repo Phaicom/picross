6c1e5ce03ec449485d60174b98e318aa952accce
Phaicom <reawpai.chun@yahoo.com>
2024-03-03 15:16:38 +0700

    chore: clean old code

--- apps/web/stores/puzzle.ts
+++ apps/web/stores/puzzle.ts
@@ -1,7 +1,7 @@
 export const usePuzzleStore = defineStore('puzzle', () => {
-  const solution = useSolutionStore()
-  const hints = useHintsStore()
-  const settings = useSettingsStore()
+  // const solution = useSolutionStore()
+  // const hints = useHintsStore()
+  // const settings = useSettingsStore()
 
   const width = ref(0)
   const height = ref(0)
@@ -9,31 +9,31 @@ export const usePuzzleStore = defineStore('puzzle', () => {
   const grid = reactive<number[][]>([])
 
   const isWin = computed(() => {
-    const trimGrid = grid.map(row => row.map(col => col === 1 ? 1 : 0))
+    // const trimGrid = grid.map(row => row.map(col => col === 1 ? 1 : 0))
 
-    return trimGrid.length > 0 && solution.grid.length > 0 && JSON.stringify(trimGrid) === JSON.stringify(solution.grid)
+    // return trimGrid.length > 0 && solution.grid.length > 0 && JSON.stringify(trimGrid) === JSON.stringify(solution.grid)
   })
 
   function generate() {
-    if (Number.isNaN(+settings.width) && Number.isNaN(+h))
-      throw new Error('width and height are required!')
-    if (settings.width <= 0 || settings.height <= 0)
-      throw new Error('width or height are less than or equal to 0!')
+    // if (Number.isNaN(+settings.width) && Number.isNaN(+h))
+    //   throw new Error('width and height are required!')
+    // if (settings.width <= 0 || settings.height <= 0)
+    //   throw new Error('width or height are less than or equal to 0!')
 
-    width.value = settings.width
-    height.value = settings.height
-    totalCells.value = width.value * height.value
+    // width.value = settings.width
+    // height.value = settings.height
+    // totalCells.value = width.value * height.value
 
-    reset()
-    solution.generate(grid)
-    hints.generate(solution.grid)
+    // reset()
+    // solution.generate(grid)
+    // hints.generate(solution.grid)
   }
 
   function reset() {
-    grid.splice(0, grid.length)
-    const arr = Array.from({ length: height.value }).fill(0).map(() => Array.from({ length: width.value }).fill(0),
-    ) as number[][]
-    Object.assign(grid, arr)
+    // grid.splice(0, grid.length)
+    // const arr = Array.from({ length: height.value }).fill(0).map(() => Array.from({ length: width.value }).fill(0),
+    // ) as number[][]
+    // Object.assign(grid, arr)
   }
 
   return { width, height, totalCells, grid, isWin, generate, reset }
--- apps/web/utils/constraint.ts
+++
@@ -1,7 +0,0 @@
-import { CellTypes } from '~/types/puzzle'
-
-export const PUZZLE_DIMENSION = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-
-export const BOARD_SIZE = [{ text: 'small', value: 6 }, { text: 'medium', value: 12 }, { text: 'large', value: 18 }]
-
-export const CELL_TYPES = [{ text: '', value: CellTypes.Empty }, { text: 'bg-cell', value: CellTypes.Fill }, { text: 'i-ph:x-light text-red', value: CellTypes.Cross }, { text: 'i-ph:circle-light text-blue', value: CellTypes.Circle }]
